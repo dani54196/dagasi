5bfc3694f8adee48d5cba52b7cc7d3021d06b517
Dani54197 <Dani54197@outlook.es>
2020-10-25 18:34:30 -0300

    Updates

---
+++ precache-manifest.780abfe0e32df3e638f4c46e99a71c60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9540be4cb51736569968abab79653a52",
+    "url": "/dagasi/index.html"
+  },
+  {
+    "revision": "0eca303f90ebe056ac43",
+    "url": "/dagasi/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "914c5e20d996c4b66658",
+    "url": "/dagasi/static/js/2.e33610c2.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/dagasi/static/js/2.e33610c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eca303f90ebe056ac43",
+    "url": "/dagasi/static/js/main.9129c222.chunk.js"
+  },
+  {
+    "revision": "802c0ce4358fc1cb4354",
+    "url": "/dagasi/static/js/runtime-main.69143f9e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dagasi/static/media/logo.5d5d9eef.svg"
+  }
+]);
